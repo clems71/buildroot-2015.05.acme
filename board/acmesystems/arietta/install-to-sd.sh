b5a32e4078b9019c1e0ed9a59226a1047cfc03dd
Clement Jacob <clems71@gmail.com>
2016-02-11 08:49:48 +0100

    Updating kernel to 4.2.6 + Fixing USB gadget on USB3 issue
    see http://marc.info/?l=linux-usb&m=144769822914087&w=2 for more info.

---
+++ board/acmesystems/arietta/install-to-sd.sh
@@ -0,0 +1,4 @@
+cp output/images/acme-arietta.dtb /media/$USER/boot/
+cp output/images/boot.bin /media/$USER/boot/
+cp output/images/zImage /media/$USER/boot/
+tar xf output/images/rootfs.tar -C /media/$USER/rootfs
---
+++ copy.sh
@@ -0,0 +1,3 @@
+cp output/images/acme-arietta.dtb /media/clement/boot/
+cp output/images/boot.bin /media/clement/boot/
+cp output/images/zImage /media/clement/boot/
