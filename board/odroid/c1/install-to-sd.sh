4f216b8a8571bef953424402cc92c777a0e561c4
Clement Jacob <clems71@gmail.com>
2016-02-27 15:01:53 +0100

    Adding support for Odroid C1

---
+++ board/odroid/c1/install-to-sd.sh
@@ -0,0 +1,4 @@
+# cp output/images/acme-arietta.dtb $MNT_DIR/boot/
+# cp output/images/boot.bin $MNT_DIR/boot/
+cp output/images/uImage /media/clement/BOOT/uImage
+tar xf output/images/rootfs.tar -C /media/clement/trusty
